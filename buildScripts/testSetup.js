a49aa6b871f35a9bcf8ea602dc2eaad2a488ec2f
Alexander Shkirya <shkirya@gmail.com>
2019-02-16 23:41:58 +0300

    Update babel

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,7 +1,7 @@
 // This file isn't transpiled, so must CommonJS and ES5
 
 // Register babel to traspile before out tests run.
-require('babel-register')();
+require('@babel/register')();
 
 // Disable webpack features that Mocha dowsn't understand.
 require.extensions['.css'] = function() {};
