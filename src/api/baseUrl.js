de33a7077eb849c140e77206c31bc3bbeaee2846
Шкиря Александр Сергеевич <shkirya_a@tass.ru>
2019-03-12 22:24:08 +0300

    End of course

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://calm-plains-22000.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
